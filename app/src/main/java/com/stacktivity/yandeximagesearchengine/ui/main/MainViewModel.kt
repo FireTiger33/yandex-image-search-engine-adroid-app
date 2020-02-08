0d91088b2d0c03c98ff9bd836bd24250b4365812
FireTiger33 <larin.grishan@yandex.ru>
2020-02-08 19:44:55 +0300

    Created parser to get list of information about images

--- app/src/main/java/com/stacktivity/yandeximagesearchengine/ui/main/MainViewModel.kt
+++ app/src/main/java/com/stacktivity/yandeximagesearchengine/ui/main/MainViewModel.kt
@@ -1,10 +1,14 @@
 package com.stacktivity.yandeximagesearchengine.ui.main
 
+import androidx.lifecycle.MutableLiveData
+import com.stacktivity.yandeximagesearchengine.util.YandexImageUtil
 import com.stacktivity.yandeximagesearchengine.base.BaseViewModel
 import com.stacktivity.yandeximagesearchengine.data.model.ImageData
 import com.stacktivity.yandeximagesearchengine.data.model.MainRepository
+import com.stacktivity.yandeximagesearchengine.data.model.SerpItem
 
 class MainViewModel : BaseViewModel() {
+    val imageListLive = MutableLiveData<List<SerpItem>>()
 
     fun fetchImages() {
         dataLoading.value = true
@@ -12,6 +16,11 @@ class MainViewModel : BaseViewModel() {
             dataLoading.value = false
             if (isSuccess) {
                 empty.value = false
+                if (response != null) {
+                    val html = response.blocks[0].html
+
+                    imageListLive.value = YandexImageUtil.getSerpListFromHtml(html)
+                }
             } else {
                 empty.value = true
             }
