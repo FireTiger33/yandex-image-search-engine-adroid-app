62016851ce29721662196078769962e32fe5871d
FireTiger33 <32743837+FireTiger33@users.noreply.github.com>
2020-08-06 17:54:15 +0300

    more accurate regular expression

--- app/src/main/java/com/stacktivity/yandeximagesearchengine/util/ImageParser.kt
+++ app/src/main/java/com/stacktivity/yandeximagesearchengine/util/ImageParser.kt
@@ -10,7 +10,7 @@ import java.net.URL
 
 class ImageParser {
     companion object {
-        private val imageLinkRegex: Regex = Regex("((https?://)?(([^\\s\"](?!https?))+?\\.(jpe?g|png|gif))+)")
+        private val imageLinkRegex: Regex = Regex("((https?:/)?(/([^\\s\"](?!https?))+(jpe?g|png|gif))+)")
         private val protocolNotFoundRegex: Regex = Regex("^/{2}")
 
         /**
