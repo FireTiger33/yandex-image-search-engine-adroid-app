110925cff1bf7c031157e6f1b1acceb20cc40818
FireTiger33 <larin.grishan@yandex.ru>
2020-11-25 20:09:26 +0300

    update for more correct operation

--- app/src/main/java/com/stacktivity/yandeximagesearchengine/util/NetworkStateReceiver.kt
+++ app/src/main/java/com/stacktivity/yandeximagesearchengine/util/NetworkStateReceiver.kt
@@ -10,21 +10,52 @@ import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import java.lang.IllegalStateException
 import java.lang.NullPointerException
-import java.util.Queue
+import java.util.Deque
 import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.ConcurrentLinkedDeque
 import kotlin.collections.HashMap
 
+/**
+ * NetworkStateReceiver v1.0
+ * Kotlin Author: Larin Grigoriy <https://github.com/FireTiger33>
+ * created March 22, 2020
+ * last updated November 25, 2020
+ *
+ * This class was created to work with Internet request queues.
+ * It allows you to optimize download of by placing priority on the most recent requests.
+ * The number of concurrent requests is set separately for each queue.
+ *
+ * This will be very useful, for example, when loading an image feed.
+ * When images are added to the download queue sequentially and scroll quickly,
+ * images in focus will be loaded immediately,
+ * instead of waiting for those that are no longer visible on the screen to load.
+ *
+ * To use this class, you need to implement your own descendant class [NetworkListener] and
+ * register this receiver in your code by calling [NetworkStateReceiver.register],
+ * passing it application context.
+ *
+ * If the Internet connection is lost, all listeners that are currently active
+ * will be notified and will be able to correctly handle this situation
+ * without reporting a failed connection to the server.
+ * When the connection is restored, they will also be notified and
+ * can continue or start downloading the necessary data again.
+ *
+ * If you are using this class in your code please add the following line:
+ *
+ * NetworkStateReceiver by Larin Grigoriy <https://github.com/FireTiger33>
+ */
 class NetworkStateReceiver private constructor(context: Context) : BroadcastReceiver() {
 
     private var cm: ConnectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    private val networkListeners: ConcurrentHashMap<String, Queue<NetworkListener>> = ConcurrentHashMap()
-    private val runningNetworkListeners: ConcurrentHashMap<String, Queue<NetworkListener>> = ConcurrentHashMap()
+    private val networkListeners: ConcurrentHashMap<String, Deque<NetworkListener>> = ConcurrentHashMap()
+    private val runningNetworkListeners: ConcurrentHashMap<String, Deque<NetworkListener>> = ConcurrentHashMap()
     private val networkListenersConf: HashMap<String, Int> = hashMapOf()
 
     abstract class NetworkListener {
         lateinit var poolTag: String
+            internal set
         var isRunning = false
+            internal set
         abstract fun onNetworkIsConnected(onSuccess: () -> Unit)
         abstract fun onNetworkIsDisconnected()
         abstract fun onCancel()
@@ -63,8 +94,8 @@ class NetworkStateReceiver private constructor(context: Context) : BroadcastRece
      * @param maxQueue - max count listeners in a pool
      */
     fun addNewPoolListeners(tag: String, maxQueue: Int) {
-        networkListeners[tag] = ConcurrentLinkedQueue()
-        runningNetworkListeners[tag] = ConcurrentLinkedQueue()
+        networkListeners[tag] = ConcurrentLinkedDeque()
+        runningNetworkListeners[tag] = ConcurrentLinkedDeque()
         networkListenersConf[tag] = maxQueue
     }
 
@@ -79,28 +110,22 @@ class NetworkStateReceiver private constructor(context: Context) : BroadcastRece
     fun addListener(tag: String, listener: NetworkListener) {
         networkListeners[tag]?.let {
             listener.poolTag = tag
-            /*if (networkListenersConf[tag] != 0 && networkListenersConf[tag] == it.size) {
-                it.remove(it.elementAt(1))
-            }*/
-
             it.add(listener)
 
-            if (it.size <= networkListenersConf[tag]!!) {
+            if (runningNetworkListeners[tag]!!.size < networkListenersConf[tag]!!) {
                 startNextListener(tag)
             }
         } ?: run { throw IllegalArgumentException("Pool $tag does not exist") }
     }
 
-    fun removeListener(tag: String, listener: NetworkListener) {
-        if (networkListeners.containsKey(tag)) {
+    fun removeListener(listener: NetworkListener) {
+        if (networkListeners.containsKey(listener.poolTag)) {
             if (listener.isRunning) {
                 listener.onCancel()
-                runningNetworkListeners[tag]!!.remove(listener)
+                runningNetworkListeners[listener.poolTag]!!.remove(listener)
             } else {
-                networkListeners[tag]!!.remove(listener)
+                networkListeners[listener.poolTag]!!.remove(listener)
             }
-        } else {
-            throw IllegalStateException("Pool $tag does not exist")
         }
     }
 
@@ -153,7 +178,7 @@ class NetworkStateReceiver private constructor(context: Context) : BroadcastRece
     }
 
     private fun startNextListener(poolTag: String) {
-        networkListeners[poolTag]?.remove()?.let {
+        networkListeners[poolTag]?.pollLast()?.let {
             it.isRunning = true
             runningNetworkListeners[poolTag]?.add(it)
             it.onNetworkIsConnected {
@@ -166,9 +191,7 @@ class NetworkStateReceiver private constructor(context: Context) : BroadcastRece
     private fun onListenerWorkComplete(listener: NetworkListener) {
         val listenerPool = listener.poolTag
         runningNetworkListeners[listenerPool]?.remove(listener)
-        if (networkListeners[listenerPool]!!.size > 0) {
-            startNextListener(listenerPool)
-        }
+        startNextListener(listenerPool)
     }
 
 }
