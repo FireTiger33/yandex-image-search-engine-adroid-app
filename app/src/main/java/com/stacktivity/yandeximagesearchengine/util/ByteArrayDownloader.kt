1b3b9d2dbc0ebee96b99f28669f04a2b05de8b64
FireTiger33 <larin.grishan@yandex.ru>
2020-11-25 19:26:19 +0300

    call handling is directly replaced with enqueue

--- app/src/main/java/com/stacktivity/yandeximagesearchengine/util/ByteArrayDownloader.kt
+++ app/src/main/java/com/stacktivity/yandeximagesearchengine/util/ByteArrayDownloader.kt
@@ -4,16 +4,20 @@ import android.util.Log
 import com.stacktivity.yandeximagesearchengine.BuildConfig.DEBUG
 import kotlinx.coroutines.*
 import okhttp3.Call
+import okhttp3.Response
+import okhttp3.Callback
 import okhttp3.EventListener
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.IOException
+import java.lang.Exception
 import java.nio.ByteBuffer
 import java.nio.channels.Channels
+import java.security.cert.CertPathValidatorException
 import java.util.concurrent.TimeUnit
 
 /**
- * Used for downloading files from the Internet in conjunction with NetworkStateReceiver
+ * Used for downloading files from the Internet in conjunction with [NetworkStateReceiver]
  *
  * @param url       - direct link to image
  * @param timeoutMs - connect timeout, can be omitted
@@ -24,19 +28,19 @@ import java.util.concurrent.TimeUnit
 class ByteArrayDownloader(
     private val url: String,
     private val timeoutMs: Long? = null,
-    private val onResult: (byteBuffer: ByteBuffer?) -> Unit
+    private val onResult: suspend (byteBuffer: ByteBuffer?) -> Unit,
 ) : NetworkStateReceiver.NetworkListener() {
     private var job: Job? = null
 
     companion object {
-        val tag = ByteArrayDownloader::class.java.simpleName
+        val tag: String = ByteArrayDownloader::class.java.simpleName
 
         private val client = OkHttpClient.Builder().apply {
             if (DEBUG) {
                 eventListener(object : EventListener() {
                     override fun callStart(call: Call) {
-                        super.callStart(call)
-                        Log.d(tag, "Load url: ${call.request().url()}")
+                        val url: String = call.request().url.toString()
+                        Log.d(tag, "Load url: $url")
                     }
 
                     override fun callFailed(call: Call, ioe: IOException) {
@@ -51,16 +55,14 @@ class ByteArrayDownloader(
     }
 
     override fun onNetworkIsConnected(onSuccess: () -> Unit) {
-        job = download(url, timeoutMs) { buffer ->
-            // TODO if gif
-            if (NetworkStateReceiver.networkIsConnected(NetworkStateReceiver.getInstance())) {
-                onResult(buffer)
-            }
+        job = CoroutineScope(SupervisorJob() + Dispatchers.IO).launch {
+            val buffer = try { download(/*url, timeoutMs*/) } catch (e: Exception) { null }
             if (buffer != null && buffer.remaining() > 0) {
                 Log.d(tag, "download complete: $url")
             } else {
                 Log.d(tag, "download failed: $url")
             }
+            onResult(buffer)
             onSuccess()
         }
     }
@@ -89,53 +91,43 @@ class ByteArrayDownloader(
      *
      * @return [Job] for task managing
      */
-    private fun download(  // todo
-        url: String,
-        timeoutMs: Long? = null,
-        onResult: (buffer: ByteBuffer?) -> Unit
-    ): Job = GlobalScope.launch(Dispatchers.IO) {
-        var result: ByteBuffer? = null
+    private suspend fun download(): ByteBuffer? = suspendCancellableCoroutine { cont ->
+        val call = getOkHttpClientWithTimeout(timeoutMs)
+            .newCall(
+                Request.Builder()
+                    .url(url)
+                    .get()
+                    .build()
+            )
 
-        try {
-            val contentLength: Long
-            val stream = getOkHttpClientWithTimeout(timeoutMs)
-                .newCall(
-                    Request.Builder()
-                        .url(url)
-                        .get()
-                        .build()
-                )
-                .await().apply {
-                    contentLength = contentLength()
-                }
-                .byteStream()
+        cont.invokeOnCancellation {
+            call.cancel()
+        }
 
-            stream.use {
-                if (contentLength < 1) return@use
-                val buffer = ByteBuffer.allocateDirect(contentLength.toInt())
-                Channels.newChannel(it).use { channel ->
-                    while (buffer.remaining() > 0) {
-                        channel.read(buffer)
+        call.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                cont.resumeWith(Result.failure(e))
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                var res: ByteBuffer? = null
+                response.body?.let { body ->
+                    val contentLength = body.contentLength().toInt()
+                    if (contentLength > 0) {
+                        body.byteStream().use {
+                            val buffer = ByteBuffer.allocateDirect(contentLength)
+                            Channels.newChannel(it).use { channel ->
+                                while (buffer.remaining() > 0) {
+                                    channel.read(buffer)
+                                }
+                            }
+                            buffer.rewind()
+                            res = buffer
+                        }
                     }
-                    result = buffer
-                    Log.i(tag, "BufferSize: ${result?.capacity()}")
                 }
+                cont.resumeWith(Result.success(res))
             }
-        } catch (e: OutOfMemoryError) {  // TODO
-            e.printStackTrace()
-            if (DEBUG) {
-                withContext(Dispatchers.Main) {
-                    shortToast("OutOfMemory")
-                }
-            }
-        } catch (t: Throwable) {
-            if (t !is CancellationException) t.printStackTrace()
-            /** Possible more exceptions:
-             * 1) SocketTimeoutException
-             * 3) CertPathValidatorException */
-        }
-        withContext(Dispatchers.Main) {
-            onResult(result)
-        }
+        })
     }
 }
