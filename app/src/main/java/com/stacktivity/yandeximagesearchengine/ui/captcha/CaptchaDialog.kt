1fcc78df46add8bcd24471ee3c0bda5a1e8434fc
FireTiger33 <larin.grishan@yandex.ru>
2020-11-25 19:14:23 +0300

    update work with captcha

--- app/src/main/java/com/stacktivity/yandeximagesearchengine/ui/captcha/CaptchaDialog.kt
+++ app/src/main/java/com/stacktivity/yandeximagesearchengine/ui/captcha/CaptchaDialog.kt
@@ -1,6 +1,7 @@
 package com.stacktivity.yandeximagesearchengine.ui.captcha
 
 import android.content.DialogInterface
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,11 +9,10 @@ import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.DialogFragment
 import com.stacktivity.yandeximagesearchengine.R
-import com.stacktivity.yandeximagesearchengine.util.ImageDownloadHelper
+import com.stacktivity.yandeximagesearchengine.util.image.BitmapObserver
+import com.stacktivity.yandeximagesearchengine.util.image.ImageLoader
+import com.stacktivity.yandeximagesearchengine.util.shortToast
 import kotlinx.android.synthetic.main.captcha_dialog.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class CaptchaDialog(
     private val imageUrl: String,
@@ -21,7 +21,7 @@ class CaptchaDialog(
 ) : DialogFragment() {
 
     companion object {
-        val tag = CaptchaDialog::class.java.simpleName
+        val tag: String = CaptchaDialog::class.java.simpleName
     }
 
     override fun onCreateView(
@@ -33,14 +33,21 @@ class CaptchaDialog(
     }
 
     private fun applyCaptchaImage(imageUrl: String) {
-        ImageDownloadHelper.getInstance().getBitmapAsync("captcha", imageUrl) { bitmap ->
-            image_captcha?.let {
-                val cropFactor = it.width.toFloat() / bitmap!!.width
-                val reqHeight = (cropFactor * bitmap.height).toInt()
-                it.layoutParams.height = reqHeight
-                it.setImageBitmap(bitmap)
+        ImageLoader.getBitmap(imageUrl, object : BitmapObserver() {
+            override fun onBitmapResult(bitmap: Bitmap) {
+                if (image_captcha != null) {
+                    val cropFactor = image_captcha.width.toFloat() / bitmap.width
+                    val reqHeight = (cropFactor * bitmap.height).toInt()
+                    image_captcha.layoutParams.height = reqHeight
+                    image_captcha.setImageBitmap(bitmap)
+                }
             }
-        }
+
+            override fun onException(e: Throwable) {
+                shortToast(R.string.failed_captcha_upload)
+            }
+
+        })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
