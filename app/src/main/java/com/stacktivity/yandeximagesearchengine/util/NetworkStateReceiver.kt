ae322460257d35bd96b49a6759138c354c2322e3
FireTiger33 <larin.grishan@yandex.ru>
2020-11-30 20:58:22 +0300

    improved algorithm for checking for an Internet connection: - used modern NetworkCapabilities instead of NetworkInfo - implemented a permanent check for Internet access

--- app/src/main/java/com/stacktivity/yandeximagesearchengine/util/NetworkStateReceiver.kt
+++ app/src/main/java/com/stacktivity/yandeximagesearchengine/util/NetworkStateReceiver.kt
@@ -1,25 +1,25 @@
-@file:Suppress("DEPRECATION")
-
 package com.stacktivity.yandeximagesearchengine.util
 
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import kotlinx.coroutines.*
 import java.lang.IllegalStateException
-import java.lang.NullPointerException
+import java.lang.Runnable
+import java.net.URL
 import java.util.Deque
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentLinkedDeque
 import kotlin.collections.HashMap
+import kotlin.coroutines.suspendCoroutine
 
 /**
- * NetworkStateReceiver v1.0
+ * NetworkStateReceiver v1.1
  * Kotlin Author: Larin Grigoriy <https://github.com/FireTiger33>
  * created March 22, 2020
- * last updated November 25, 2020
+ * last updated November 30, 2020
  *
  * This class was created to work with Internet request queues.
  * It allows you to optimize download of by placing priority on the most recent requests.
@@ -30,9 +30,8 @@ import kotlin.collections.HashMap
  * images in focus will be loaded immediately,
  * instead of waiting for those that are no longer visible on the screen to load.
  *
- * To use this class, you need to implement your own descendant class [NetworkListener] and
- * register this receiver in your code by calling [NetworkStateReceiver.register],
- * passing it application context.
+ * To full use this class, you need to implement your own descendant class [NetworkListener]
+ * and register this receiver in your code by calling [NetworkStateReceiver.register].
  *
  * If the Internet connection is lost, all listeners that are currently active
  * will be notified and will be able to correctly handle this situation
@@ -44,12 +43,25 @@ import kotlin.collections.HashMap
  *
  * NetworkStateReceiver by Larin Grigoriy <https://github.com/FireTiger33>
  */
-class NetworkStateReceiver private constructor(context: Context) : BroadcastReceiver() {
+class NetworkStateReceiver private constructor(context: Context) {
 
-    private var cm: ConnectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    private val networkListeners: ConcurrentHashMap<String, Deque<NetworkListener>> = ConcurrentHashMap()
-    private val runningNetworkListeners: ConcurrentHashMap<String, Deque<NetworkListener>> = ConcurrentHashMap()
+    // test host for checking Internet connection
+    var testHost get() = NetworkStateChecker.testHost
+        set(value) { NetworkStateChecker.testHost = value }
+
+    var networkIsConnected: Boolean = false
+        private set
+
+    private val networkListeners: ConcurrentHashMap<String, Deque<NetworkListener>> =
+        ConcurrentHashMap()
+    private val runningNetworkListeners: ConcurrentHashMap<String, Deque<NetworkListener>> =
+        ConcurrentHashMap()
     private val networkListenersConf: HashMap<String, Int> = hashMapOf()
+
+    init {
+        registerNetworkCallback(context)
+        addNewPoolListeners(tag, 3)  // pool listeners for post method
+    }
 
     abstract class NetworkListener {
         lateinit var poolTag: String
@@ -57,8 +69,8 @@ class NetworkStateReceiver private constructor(context: Context) : BroadcastRece
         var isRunning = false
             internal set
         abstract fun onNetworkIsConnected(onSuccess: () -> Unit)
-        abstract fun onNetworkIsDisconnected()
-        abstract fun onCancel()
+        open fun onNetworkIsDisconnected() { }
+        open fun onCancel() { }
     }
 
     companion object {
@@ -73,17 +85,83 @@ class NetworkStateReceiver private constructor(context: Context) : BroadcastRece
         fun getInstance(): NetworkStateReceiver {
             return INSTANCE ?: throw NullPointerException("Receiver is not registered")
         }
+    }
 
-        fun networkIsConnected(networkStateReceiver: NetworkStateReceiver): Boolean {
-            val info: NetworkInfo? = networkStateReceiver.cm.activeNetworkInfo
-            return info != null && info.isConnected
+    private object NetworkStateChecker {
+
+        var testHost = "https://www.ya.ru"
+            internal set(value) {
+                field = value
+                restart()
+            }
+
+        private val networkCheckScope = CoroutineScope(Dispatchers.IO)
+        private var isStarted = false
+
+        fun start() {
+            if (!isStarted) {
+                isStarted = true
+                networkCheckScope.launch {
+                    while (true) {
+                        getInstance().applyNetworkStatus(networkAccessAvailable())
+                        delay(3000)
+                    }
+                }
+            }
+        }
+
+        fun stop() {
+            networkCheckScope.coroutineContext.cancelChildren()
+            isStarted = false
+        }
+
+        fun restart() {
+            stop()
+            start()
+        }
+
+        private suspend fun connectToTestHostAsync() = coroutineScope {
+            async(Dispatchers.IO) {
+                URL(testHost).openConnection().apply {
+                    connectTimeout = 3000
+                    readTimeout = 1000
+                }.connect()
+            }
+        }
+
+        private suspend fun networkAccessAvailable() = suspendCoroutine<Boolean> {
+            networkCheckScope.launch {
+                try {
+                    connectToTestHostAsync().await()
+                    it.resumeWith(Result.success(true))
+                } catch (e: Exception) {
+                    it.resumeWith(Result.success(false))
+                }
+            }
         }
     }
 
-    init {
-        context.applicationContext.registerReceiver(this,
-            IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
-        )
+
+    /**
+     * if you have Internet access, it starts [task],
+     * else [task] added to the default pool listeners.
+     *
+     * Do not use this method to load a large amount of data,
+     * due to the limitation of simultaneous tasks (by default, 3).
+     * To change it, use [NetworkStateReceiver.changeMaxQueueForPool]
+     * with [NetworkStateReceiver.tag]
+     */
+    fun post(task: Runnable) {
+        if (networkIsConnected) {
+            task.run()
+        } else {
+            addListener(tag, object : NetworkListener() {
+                override fun onNetworkIsConnected(onSuccess: () -> Unit) {
+                    task.run()
+                    onSuccess()
+                }
+            })
+        }
     }
 
     /**
@@ -112,7 +190,8 @@ class NetworkStateReceiver private constructor(context: Context) : BroadcastRece
             listener.poolTag = tag
             it.add(listener)
 
-            if (runningNetworkListeners[tag]!!.size < networkListenersConf[tag]!!) {
+            if (runningNetworkListeners[tag]!!.size < networkListenersConf[tag]!!
+                && networkIsConnected) {
                 startNextListener(tag)
             }
         } ?: run { throw IllegalArgumentException("Pool $tag does not exist") }
@@ -153,26 +232,34 @@ class NetworkStateReceiver private constructor(context: Context) : BroadcastRece
 
     fun getListenersCountByTag(tag: String): Int? = networkListeners[tag]?.size
 
-    override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == ConnectivityManager.CONNECTIVITY_ACTION) {
-            val poolTags = networkListeners.keys
+    private fun applyNetworkStatus(isConnected: Boolean) {
+        if (networkIsConnected != isConnected) {
+            networkIsConnected = isConnected
 
-            if (networkIsConnected(this)) {
-                poolTags.forEach { tag ->
-                    // start work
-                    runningNetworkListeners[tag]?.forEach {
-                        it.onNetworkIsConnected {
-                            onListenerWorkComplete(it)
-                        }
-                    }
-                    startNextListener(tag)
-                }
+            if (isConnected) {
+                notifyNetworkIsConnected()
             } else {
-                poolTags.forEach { tag ->
-                    runningNetworkListeners[tag]?.forEach { listener ->
-                        listener.onNetworkIsDisconnected()
-                    }
+                notifyNetworkIsDisconnected()
+            }
+        }
+    }
+
+    private fun notifyNetworkIsConnected() {
+        networkListeners.keys.forEach { tag ->
+            // start work
+            runningNetworkListeners[tag]?.forEach {
+                it.onNetworkIsConnected {
+                    onListenerWorkComplete(it)
                 }
+            }
+            startNextListener(tag)
+        }
+    }
+
+    private fun notifyNetworkIsDisconnected() {
+        networkListeners.keys.forEach { tag ->
+            runningNetworkListeners[tag]?.forEach { listener ->
+                listener.onNetworkIsDisconnected()
             }
         }
     }
@@ -194,4 +281,42 @@ class NetworkStateReceiver private constructor(context: Context) : BroadcastRece
         startNextListener(listenerPool)
     }
 
+    private fun registerNetworkCallback(context: Context) {
+        val cm: ConnectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val request = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+            .build()
+
+        cm.registerNetworkCallback(request, getNetworkCallbackListener())
+    }
+
+    private fun getNetworkCallbackListener(): ConnectivityManager.NetworkCallback {
+        return object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                NetworkStateChecker.start()
+                applyNetworkStatus(true)
+            }
+
+            override fun onLost(network: Network) {
+                NetworkStateChecker.stop()
+                applyNetworkStatus(false)
+            }
+
+            override fun onUnavailable() {
+                NetworkStateChecker.stop()
+                applyNetworkStatus(false)
+                // TODO notify that Internet is not expected and show reload btn
+            }
+
+            override fun onBlockedStatusChanged(network: Network, blocked: Boolean) {
+                applyNetworkStatus(!blocked)
+                if (blocked) NetworkStateChecker.stop()
+                else NetworkStateChecker.start()
+                // todo notify
+            }
+        }
+    }
 }
